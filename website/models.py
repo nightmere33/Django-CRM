300efae6fde76da87f9f1e805e81eaa1ba6a0483
younes ouali <younesoft2017@gmail.com>
2025-04-23 21:08:51 +0100

    database model added

--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#we can call it customers or anything we want
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True) #this will add the date and time when the record is created
+    first_name = models.CharField(max_length=25) #max length of the first name is 100
+    last_name = models.CharField(max_length=25) #max length of the last name is 100
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    adress = models.CharField(max_length=155)
+    city = models.CharField(max_length=50) 
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}") #this will return the first name and last name of the record when we call it in the admin panel
+
+# and to put this model to database u must migrate using  python manage.py makemigrations
+# and then push it using python manage.py migrate
--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+
 # Create your views here.
 def home(request):
     #check if user is ligging in 
@@ -28,4 +30,20 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html',{})
+    if request.method =='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authunticate and login
+            username = form.cleaned_data.get('username') #take the username entered
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username = username,password=password)
+            login(request,user)
+            messages.success(request,f"{user.username} You Have Successfully Registered ")
+
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html',{'form':form}) #we passed the form to the html page and use it
+    
+    return render(request, 'register.html',{'form':form})
